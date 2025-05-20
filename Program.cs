f2570c24620898ec3f33d2cb67b7226ba647e2e2
TheVikasPawar <vikaspawar.connect@gmail.com>
2025-05-20 22:32:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Program
+{
+    class First
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Data Type
+            byte  -127 to 128  1byte
+            short 2 byte
+            int 2 byte
+            long 4 byte
+
+            flaot   4 byte 7 -8 digit precision
+            decimal 4 byte 15- 16 digits
+            double  8 byte 28-29
+
+            char     2 byte
+            string   varies
+            
+            Datetime 
+
+            var
+            */
+            //  byte Byte = 10;
+            // short Short = 500;
+            // int Int = 29857894;
+            // long Long = 78665894654685;
+            // float Float = 25.56324656f;
+            // double Double = 651554.65746574848d;
+            // decimal Decimal = 64546565.6454645487456654m;
+            // char Char = 'C';
+            // string name = "Vikas";
+            // DateTime currentTime = DateTime.Now;
+            // var vikas = "Vikas";
+            // bool IsActive = false;
+            // Console.WriteLine("Byte >>>" + Byte);
+            // Console.WriteLine("Short >>>" + Short);
+            // Console.WriteLine("Int >>>" + Int);
+            // Console.WriteLine("Long >>>" + Long);
+            // Console.WriteLine("Float >>>" + Float);
+            // Console.WriteLine("Double >>>" + Double);
+            // Console.WriteLine("Decimal >>>" + Decimal);
+            // Console.WriteLine("Char >>>" + Char);
+            // Console.WriteLine("name >>>" + name);
+            // Console.WriteLine("currentTime >>>" + currentTime);
+            // Console.WriteLine("var >>>" + vikas);
+            // Console.WriteLine("isActive >>>" + IsActive);
+            // Console.WriteLine("Hello Vikas👋");
+
+            //Video 3
+
+        }
+    }
+}
